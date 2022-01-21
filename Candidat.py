f1dd396575e9530b22233c68e7b042d8901f2656
temistocle1998 <laminebeye007@gmail.com>
2022-01-21 04:12:23 +0000

    Update Candidat.py

--- Candidat.py
+++ Candidat.py
@@ -1,133 +1,222 @@
-from glob import glob
+import tkinter as tk
+from tkinter import ttk, messagebox
+import mysql.connector
 from tkinter import *
-from  tkinter import  ttk
-#importing connection
-import  mysql.connector
-#establishing connection
-conn = mysql.connector.connect(
-   user='phpmyadmin', password='djarafat10', host='localhost', database='python_poo')
 
-#defining register function
-def register():
-    #getting form data
-    numero_table1=numero_table.get()
-    prenom_s1=prenom_s.get()
-    nom1=nom.get()
-    date_naissance1=date_naissance.get()
-    lieu_naissance1=lieu_naissance.get()
-    sexe1=sexe.get()
-    nationalite1=nationalite.get()
-    choix_epr_facultative1 = choix_epr_facultative.get()
-    epreuve_facultative1 = epreuve_facultative.get()
-    aptitude_sportive1 = aptitude_sportive.get()
-    #applying empty validation
-    if numero_table1=='' or prenom_s1==''or nom1=='' or date_naissance1==''or lieu_naissance1==''or sexe1=='':
-        message.set("remplissez tous les champs!!!")
-    else:
-       # Creating a cursor object using the cursor() method
-       cursor = conn.cursor()
-       # Preparing SQL query to INSERT a record into the database.
-       insert_stmt = (
-           "INSERT INTO jury(numero_table, prenom_s, nom, date_naissance, lieu_naissance, sexe, nationalite, choix_epr_facultative, epreuve_facultative, aptitude_sportive)"
-           "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
-    #    if gen1==1:
-    #     data = (numero_table1, con1,nom1,"Male",lieu_naissance1,sexe1)
-    #    else:
-    #     data = (numero_table1, con1, nom1, "Female", lieu_naissance1, sexe1)
-       data = (numero_table1, prenom_s1, nom1, date_naissance1, lieu_naissance1, sexe1, nationalite1, choix_epr_facultative1, epreuve_facultative1, aptitude_sportive1)
-       try:
-           #executing the sql command
-           cursor.execute(insert_stmt,data)
-           #commit changes in database
-           conn.commit()
-       except:
-           conn.rollback()
-       message.set("Stored successfully")
+from DB import DB
 
-#defining Registrationform function
-def Registrationform():
-    global reg_screen
-    reg_screen = Tk()
-    #Setting title of screen
-    reg_screen.title("Registration Form")
-    #setting height and width of screen
-    reg_screen.geometry("750x400")
-    #declaring variable
-    global  message;
-    global numero_table
-    global prenom_s
-    global nom
-    global date_naissance
-    global lieu_naissance
-    global sexe
-    global nationalite
-    global choix_epr_facultative
-    global aptitude_sportive
-    global epreuve_facultative
-    numero_table = StringVar()
-    prenom_s = StringVar()
-    nom=StringVar()
-    date_naissance=StringVar()
-    lieu_naissance=StringVar()
-    sexe=StringVar()
-    nationalite = StringVar()
-    choix_epr_facultative = StringVar()
-    epreuve_facultative = StringVar()
-    aptitude_sportive = StringVar()
-    message=StringVar()
-    #Creating layout of Registration form
-    Label(reg_screen,width="300", text="Please enter details below", bg="orange",fg="white")
-    #numero_table Label
-    Label(reg_screen, text="numero_table * ").grid(row = 0,column = 0)
-    #numero_table textbox
-    Entry(reg_screen, textvariable=numero_table).grid(row = 0,column = 1)
-    #prenom_s Label
-    Label(reg_screen, text="prenom_s * ").grid(row = 1,column = 0)
-    #prenom_s textbox
-    Entry(reg_screen, textvariable=prenom_s).grid(row = 1,column = 1)
+def GetValue(event):
+    e1.delete(0, END)
+    e2.delete(0, END)
+    e3.delete(0, END)
+    e4.delete(0, END)
+    e5.delete(0, END)
+    e6.delete(0, END)
+    e7.delete(0, END)
+    e8.delete(0, END)
+    e9.delete(0, END)
+    e10.delete(0, END)
+    row_id = listBox.selection()[0]
+    select = listBox.set(row_id)
+    e0.insert(0,select['id'])
+    e1.insert(0,select['numero_table'])
+    e2.insert(0,select['prenom_s'])
+    e3.insert(0,select['nom'])
+    e4.insert(0,select['date_naissance'])
+    e5.insert(0,select['lieu_naissance'])
+    e6.insert(0,select['sexe'])
+    e7.insert(0,select['nationalite'])
+    e8.insert(0,select['choix_epr_facultative'])
+    e9.insert(0,select['epreuve_facultative'])
+    e10.insert(0,select['aptitude_sportive'])
 
-    # nom Label
-    Label(reg_screen, text="nom * ").grid(row = 2,column = 0)
-    # nom textbox
-    Entry(reg_screen, textvariable=nom).grid(row = 2,column = 1)
 
-    # date_naissance Label
-    Label(reg_screen, text="date de naissance * ").grid(row = 3,column = 0)
-    # date_naissance radiobutton
-    Entry(reg_screen, textvariable=date_naissance).grid(row = 3,column = 1)
+def Add():
+    studid = e1.get()
+    studname = e2.get()
+    coursename = e3.get()
+    feee = e4.get()
+    see = e5.get()
+    tee = e6.get()
+    lee = e7.get()
+    dee = e8.get()
+    ree = e9.get()
+    vee = e10.get()
+    mysqldb=mysql.connector.connect(host="localhost",user="phpmyadmin",password="djarafat10",database="python_poo")
+    mycursor=mysqldb.cursor()
 
-    Label(reg_screen, text="lieu de naissance * ").grid(row = 4,column = 0)
-    # lieu_naissance combobox
-    Entry(reg_screen, textvariable=lieu_naissance).grid(row = 4,column = 1)
-    # sexe Label
-    Label(reg_screen, text="sexe * ").grid(row = 5,column = 0)
-    Radiobutton(reg_screen,text="Male",variable=sexe,value="Masculin").grid(row = 5,column = 1)
-    Radiobutton(reg_screen, text="Female", variable=sexe, value="Feminin").grid(row = 5,column = 2)
-    # sexe combobox
-    Label(reg_screen, text="nationalite * ").grid(row = 6,column = 0)
-    monthchoosen = ttk.Combobox(reg_screen, width=27, textvariable=nationalite)
-    monthchoosen['values'] = (' Senegalaise',
-                              ' Guinneene',
-                              ' Malienne',)
-    monthchoosen.current()
-    monthchoosen.grid(row = 6,column = 1)
+    try:
+       sql = "INSERT INTO  candidat (numero_table, prenom_s, nom, date_naissance, lieu_naissance, sexe, nationalite, choix_epr_facultative, epreuve_facultative, aptitude_sportive) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+       val = (studid,studname,coursename,feee, see, tee, lee, dee, ree, vee)
+       mycursor.execute(sql, val)
+       mysqldb.commit()
+       lastid = mycursor.lastrowid
+       messagebox.showinfo("information", "Candidat enregistré...")
+       e1.delete(0, END)
+       e2.delete(0, END)
+       e3.delete(0, END)
+       e4.delete(0, END)
+       e5.delete(0, END)
+       e6.delete(0, END)
+       e7.delete(0, END)
+       e8.delete(0, END)
+       e9.delete(0, END)
+       e10.delete(0, END)
+       e1.focus_set()
+    except Exception as e:
+       print(e)
+       mysqldb.rollback()
+       mysqldb.close()
 
-    Label(reg_screen, text="Apte * ").grid(row = 7,column = 0)
-    Radiobutton(reg_screen,text="oui",variable=aptitude_sportive,value="1").grid(row = 7,column = 1)
-    Radiobutton(reg_screen, text="non", variable=aptitude_sportive, value="0").grid(row = 7,column = 2)
 
-    Label(reg_screen, text="choix epreuve facultative ").grid(row = 8,column = 0)
-    # lieu_naissance combobox
-    Radiobutton(reg_screen,text="oui",variable=choix_epr_facultative,value="1").grid(row = 8,column = 1)
-    Radiobutton(reg_screen, text="non", variable=choix_epr_facultative, value="0").grid(row = 8,column = 2)
+def update():
+    id= e0.get()
+    studid = e1.get()
+    studname = e2.get()
+    coursename = e3.get()
+    feee = e4.get()
+    see=e5.get()
+    tee=e6.get()
+    lee=e7.get()
+    dee=e8.get()
+    ree=e9.get()
+    vee=e10.get()
+    mysqldb=mysql.connector.connect(host="localhost",user="phpmyadmin",password="djarafat10",database="python_poo")
+    mycursor=mysqldb.cursor()
 
-    Label(reg_screen, text="epreuve facultative ").grid(row = 9,column = 0)
-    # lieu_naissance combobox
-    Entry(reg_screen, textvariable=epreuve_facultative).grid(row = 9,column = 1)
-    #Label for displaying login status[success/failed]
-    Label(reg_screen, text="",textvariable=message).place(x=95,y=264)
-    #Login button
-    Button(reg_screen, text="Register", width=10, height=1, bg="orange",command=register).place(x=105,y=300)
-    reg_screen.mainloop()
-#calling function Registrationform
-Registrationform()
+    try:
+       sql = "Update  candidat set numero_table=%s, prenom_s=%s, nom=%s, date_naissance=%s, lieu_naissance=%s, sexe=%s, nationalite=%s, choix_epr_facultative=%s, epreuve_facultative=%s, aptitude_sportive=%s where id = %s"
+       val = (studid,studname,coursename,feee, see, tee, lee, dee, ree, vee, id)
+       mycursor.execute(sql, val)
+       mysqldb.commit()
+       lastid = mycursor.lastrowid
+       messagebox.showinfo("information", "Candidat à jour...")
+       print(lastid)
+       e1.delete(0, END)
+       e2.delete(0, END)
+       e3.delete(0, END)
+       e4.delete(0, END)
+       e1.focus_set()
+
+    except Exception as e:
+
+       print(e)
+       mysqldb.rollback()
+       mysqldb.close()
+
+def delete():
+    id = e0.get()
+
+    mysqldb=mysql.connector.connect(host="localhost",user="phpmyadmin",password="djarafat10",database="python_poo")
+    mycursor=mysqldb.cursor()
+
+    try:
+       sql = "DELETE FROM `candidat` WHERE id= ?"
+       val = (id)
+       mycursor.execute(sql, (id,))
+       mysqldb.commit()
+       lastid = mycursor.lastrowid
+       messagebox.showinfo("information", "Candidat supprimé ...")
+       print(val)
+       e1.delete(0, END)
+       e2.delete(0, END)
+       e3.delete(0, END)
+       e4.delete(0, END)
+       e5.delete(0, END)
+       e6.delete(0, END)
+       e7.delete(0, END)
+       e8.delete(0, END)
+       e9.delete(0, END)
+       e10.delete(0, END)
+       e1.focus_set()
+
+    except Exception as e:
+
+       print(e)
+       mysqldb.rollback()
+       mysqldb.close()
+
+def show():
+        mysqldb = mysql.connector.connect(host="localhost", user="phpmyadmin", password="djarafat10", database="python_poo")
+        mycursor = mysqldb.cursor()
+        mycursor.execute("SELECT id, numero_table,prenom_s,nom,date_naissance, lieu_naissance, sexe, nationalite, choix_epr_facultative, epreuve_facultative, aptitude_sportive FROM candidat")
+        records = mycursor.fetchall()
+        print(records)
+
+        for i, (id,stname, course,fee, three, elem, epr, apt, fac, test, sec) in enumerate(records, start=1):
+            listBox.insert("", "end", values=(id, stname, course, fee, three, elem, epr, apt, fac, test, sec))
+            mysqldb.close()
+
+root = Tk()
+root.geometry("1200x760")
+global e0
+global e1
+global e2
+global e3
+global e4
+global e5
+global e6
+global e7
+global e8
+
+tk.Label(root, text="Candidat", fg="red", font=(None, 30)).place(x=400, y=5)
+Label(root, text="Numero_table").grid(row = 1,column = 0)
+Label(root, text="prenom").grid(row = 2,column = 0)
+Label(root, text="nom").grid(row = 3,column = 0)
+Label(root, text="date de naissance").grid(row = 4,column = 0)
+Label(root, text="lieu de naissance").grid(row = 5,column = 0)
+Label(root, text="nationalite").grid(row = 6,column = 0)
+Label(root, text="sexe").grid(row = 7,column = 0)
+Label(root, text="choix epreuve").grid(row = 8,column = 0)
+Label(root, text="epreuve fac").grid(row = 9,column = 0)
+Label(root, text="aptitude").grid(row = 10,column = 0)
+
+e0 = Entry(root)
+e0.grid(row = 1,column = 1)
+
+e1 = Entry(root)
+e1.grid(row = 1,column = 1)
+
+e2 = Entry(root)
+e2.grid(row = 2,column = 1)
+
+e3 = Entry(root)
+e3.grid(row = 3,column = 1)
+
+e4 = Entry(root)
+e4.grid(row = 4,column = 1)
+e5 = Entry(root)
+
+e5.grid(row = 5,column = 1)
+
+e6 = Entry(root)
+e6.grid(row = 6,column = 1)
+
+e7 = Entry(root)
+e7.grid(row = 7,column = 1)
+e8 = Entry(root)
+
+e8.grid(row = 8,column = 1)
+e9 = Entry(root)
+
+e9.grid(row = 9,column = 1)
+
+e10 = Entry(root)
+
+e10.grid(row = 10,column = 1)
+
+Button(root, text="ajout",command = Add,height=3, width= 13).grid(row = 11,column = 1)
+Button(root, text="modifier",command = update,height=3, width= 13).grid(row = 11,column = 2)
+Button(root, text="supprimer",command = delete,height=3, width= 13).grid(row = 11,column = 3)
+
+cols = ('id','numero_table', 'prenom_s', 'nom','date_naissance', 'lieu_naissance', 'sexe', 'nationalite', 'choix_epr_facultative', 'epreuve_facultative', 'aptitude_sportive')
+listBox = ttk.Treeview(root, columns=cols, show='headings')
+
+for col in cols:
+    listBox.heading(col, text=col)
+    listBox.grid(row=12, column=0, columnspan=1)
+    listBox.place(x=10, y=400)
+show()
+listBox.bind('<Double-Button-1>',GetValue)
+
+root.mainloop()
